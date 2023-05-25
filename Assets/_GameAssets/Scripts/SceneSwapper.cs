f824261b73110b5b0be25f10bef4be38e589e08a
Matej Koncos <koncosm@msoe.edu>
2023-05-24 21:49:24 -0500

    Final push

--- Assets/_GameAssets/Scripts/SceneSwapper.cs
+++ Assets/_GameAssets/Scripts/SceneSwapper.cs
@@ -11,12 +11,27 @@ public class SceneSwapper : MonoBehaviour
     {
         if (Enemy.tag == "Player")
         {
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(Fade());
         }
     }
 
     public void SwapScenes(string sceneName)
     {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator Fade()
+    {
+        float ANIMATION_TIME = 1.5f;
+        int ANIMATION_STEPS = 50;
+
+        for (float l = 0.0f; l < 1.0f; l += 1.0f / ANIMATION_STEPS)
+        {
+            Camera.main.GetComponent<AudioSource>().volume -= ANIMATION_TIME/ANIMATION_STEPS;
+            Camera.main.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.Lerp(Color.clear, Color.black, l);
+            yield return new WaitForSeconds(ANIMATION_TIME / ANIMATION_STEPS);
+        }
+
         SceneManager.LoadScene(sceneName);
     }
 }
--- Assets/_GameAssets/Scripts/TeleportTrigger.cs
+++ Assets/_GameAssets/Scripts/TeleportTrigger.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class TeleportTrigger : MonoBehaviour
 {
+    private AudioSource door;
     // Start is called before the first frame update
     void Start()
     {
-        
+        door = GetComponents<AudioSource>()[0];
     }
 
     // Update is called once per frame
@@ -21,10 +22,25 @@ public class TeleportTrigger : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))//compare tag TeleportTrigger
         {
-            SceneManager.LoadScene("Level2");
-            Debug.Log("Loaded Next Scene");
+            StartCoroutine(Fade());
             //if collide with object that has tag "TeleportTrigger"
             //then load the next scene//
         }
+    }
+
+    IEnumerator Fade()
+    {
+        door.Play();
+        // yield return new WaitForSeconds(1);
+        float ANIMATION_TIME = 1.5f;
+        int ANIMATION_STEPS = 50;
+
+        for (float l = 0.0f; l < 1.0f; l += 1.0f / ANIMATION_STEPS)
+        {
+            Camera.main.GetComponent<AudioSource>().volume -= ANIMATION_TIME/ANIMATION_STEPS;
+            Camera.main.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.Lerp(Color.clear, Color.black, l);
+            yield return new WaitForSeconds(ANIMATION_TIME / ANIMATION_STEPS);
+        }
+        SceneManager.LoadScene("Level2");
     }
 }
